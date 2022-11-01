7d13adbf22c5e0ae5cbab99fc4c3079c9811c9a5
Yonas44 <tesfu.yonas1350@gmail.com>
2022-11-01 18:11:50 +0300

    Use ES6 modules to dynamically render tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './style.css';
+import TaskLoader from './modules/taskLoader.js';
+
+const tasks = [
+  {
+    string: 'Wash the dishes',
+    bool: false,
+    number: 0,
+  },
+  {
+    string: 'Complete To Do list project',
+    bool: false,
+    number: 1,
+  },
+];
 
 const input = document.querySelector('#input');
 const form = document.querySelector('#second');
@@ -6,3 +20,6 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   input.value = '';
 });
+
+const lists = document.querySelector('.list');
+TaskLoader(lists, tasks);
